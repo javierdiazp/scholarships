397221f2d0bebf7f546bc71dd82dac8db1a50d14
Javier Diaz <javier.diaz@capitalizarme.com>
2022-01-01 14:51:05 -0300

    Remove app loans

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
 
     # Project apps
     'accounts',
-    'loans',
 ]
 
 MIDDLEWARE = [
--- loans/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from loans.views import LoanViewSet
-
-router = DefaultRouter()
-router.register(r'', LoanViewSet, basename='loan')
-urlpatterns = router.urls
